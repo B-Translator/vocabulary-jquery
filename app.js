c2f501d58b84cc025ab90926ff6923532bd0fd8b
Dashamir Hoxha <dashohoxha@gmail.com>
2014-12-24 08:50:50 +0100

    Added custom css and js files (app.css and app.js).
    Added a favicon.
    
    Added the search input example from:
     - http://demos.jquerymobile.com/1.4.5/listview-autocomplete-remote/

---
+++ app.js
@@ -0,0 +1,31 @@
+
+$(document).on("pagecreate", "#myPage", function() {
+    $("#autocomplete").on("filterablebeforefilter", function (e, data) {
+        console.log('TEST');
+        var $ul = $(this),
+        $input = $(data.input),
+        value = $input.val(),
+        html = "";
+        $ul.html("");
+        if (value && value.length > 2) {
+            $ul.html("<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>");
+            $ul.listview("refresh");
+            $.ajax({
+                url: "http://gd.geobytes.com/AutoCompleteCity",
+                dataType: "jsonp",
+                crossDomain: true,
+                data: {
+                    q: $input.val()
+                }
+            })
+		.then(function (response) {
+                    $.each(response, function (i, val) {
+			html += "<li>" + val + "</li>";
+                    });
+                    $ul.html(html);
+                    $ul.listview("refresh");
+                    $ul.trigger("updatelayout");
+		});
+        }
+    });
+});
