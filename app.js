0a8a29cdae440d78555d4452e8dbc94e4d13f0e0
Dashamir Hoxha <dashohoxha@gmail.com>
2014-12-27 07:00:30 +0100

    Reorganizing js functions.

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 
+/**
+ * This function is called when a term from the autocomplete list is
+ * selected (clicked).
+ */
+var select_term = function (e) {
+    // Set the selected term to the search input.
+    var term = $(this).html();
+    $('#autocomplete-input')[0].value = term;
+
+    // Delete the autocomplete list.
+    var $ul = $("#autocomplete");
+    $ul.html('');
+    $ul.listview("refresh");
+    $ul.trigger("updatelayout");
+}
+
 $(document).on("pagecreate", "#vocabulary", function() {
 
     $("#autocomplete").on("filterablebeforefilter", function (e, data) {
-        var $ul = $(this),
-        $input = $(data.input),
-        value = $input.val(),
-        html = "";
+        var $ul = $(this);
+        var $input = $(data.input);
+        var value = $input.val();
+        var html = "";
         $ul.html("");
         if (value && value.length > 1) {
             $ul.html("<li><div class='ui-loader'><span class='ui-icon ui-icon-loading'></span></div></li>");
@@ -19,17 +35,11 @@ $(document).on("pagecreate", "#vocabulary", function() {
 			html += '<li><a href="#" class="term">' + val + '</a></li>';
                     });
                     $ul.html(html);
-
-		    $(".term").click(function (event) {
-			var term = $(this).html();
-			$('#autocomplete-input')[0].value = term;
-			$ul.html('');
-			$ul.listview("refresh");
-			$ul.trigger("updatelayout");
-		    });
-
                     $ul.listview("refresh");
                     $ul.trigger("updatelayout");
+
+		    // When a term from the list is clicked, select that term.
+		    $(".term").on('click', select_term);
 		});
         }
     });
