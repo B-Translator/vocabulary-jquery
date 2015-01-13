6e78c6cbf9a55a8002f1ddb9e733df45647aef17
Dashamir Hoxha <dashohoxha@gmail.com>
2015-01-13 10:47:19 +0100

    Fixed the base_url.

--- js/api-config.js
+++ js/api-config.js
@@ -2,8 +2,7 @@
 /**
  * Base URL of the server that offers the API.
  */
-//var base_url = 'https://btranslator.org';
-var base_url = 'http://dev.btranslator.org';
+var base_url = 'https://btranslator.org';
 
 /**
  * Extend the function http_request().
