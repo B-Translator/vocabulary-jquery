514a100568a34f4c67c8bf9d2b247417b6122d85
Dashamir Hoxha <dashohoxha@gmail.com>
2015-02-19 09:43:41 +0100

    Browsers usually block popup windows (used by the proxy auth_flow).

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@ var $base_url = 'https://btranslator.org';
 /** Settings for oauth2 authentication. */
 var $oauth2_settings = {
     app_id: $app_id,
-    auth_flow: 'proxy',                  // password | proxy
+    auth_flow: 'password',                  // password | proxy
     proxy_url: 'https://l10n.org.al/user/oauth2_login?proxy=true',
     token_endpoint: $base_url + '/oauth2/token',
     client_id: 'vocabulary-jquery-ict-sq',
