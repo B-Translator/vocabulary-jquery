bc677f2d87d18ba9eee64b2a690e9e578eb3ac62
Dashamir Hoxha <dashohoxha@gmail.com>
2015-02-22 11:18:53 +0100

    Extracted the code of menu on a separate file.

--- app.js
+++ app.js
@@ -6,6 +6,52 @@ var $app = (function () {
     });
 
     
+var _menu = {
+    init: function () {
+        _menu.update();
+
+        $('#menuButton').on('click', function() {
+            if ($user.isLoged()) {
+                $('#login').hide();
+                $('#logout').show();
+            }
+            else {
+                $('#login').show();
+                $('#logout').hide();
+            }
+        });
+    },
+
+    update: function () {
+        // Render the menu template.
+        var menu_tmpl = $('#tmpl-menu').html();
+        var data = {
+            base_url: $base_url,
+            lng: $lng,
+            vocabulary: $vocabulary,
+            webapp_url: $webapp_url,
+        };
+        $("#popupMenu")
+            .html(Mustache.render(menu_tmpl, data))
+            .enhanceWithin().popup();
+
+
+        $('#login').on('click', function () {
+            $user.login();
+        });
+
+        $('#logout').on('click', function () {
+            $user.logout();
+        });
+
+        // Close the menu when an item is clicked.
+        $('#popupMenu li').on('click', function() {
+            $('#popupMenu').popup('close');
+        });
+    },
+};
+
+    
 /**
  * When the page with id 'vocabulary' is created,
  * do the things that are listed in the function.
@@ -21,7 +67,7 @@ $(document).on('pagecreate', '#vocabulary', function() {
     });
 
     // Setup menu items.
-    menu_setup();
+    _menu.init();
 
     // Add a new term when the button is clicked.
     $('#add-new-term').on('click', _term.add);
@@ -38,48 +84,6 @@ $(document).on('pagecreate', '#vocabulary', function() {
     // Initialize Disqus.
     $disqus_shortname && _disqus.init($disqus_shortname);
 });
-
-/**
- * Setup menu items.
- */
-var menu_setup = function () {
-    // Render the menu template.
-    var menu_tmpl = $('#tmpl-menu').html();
-    var data = {
-        base_url: $base_url,
-        lng: $lng,
-        vocabulary: $vocabulary,
-        webapp_url: $webapp_url,
-    };
-    $("#popupMenu")
-        .html(Mustache.render(menu_tmpl, data))
-        .enhanceWithin().popup();
-
-    $('#menuButton').on('click', function() {
-        if ($user.isLoged()) {
-            $('#login').hide();
-            $('#logout').show();
-        }
-        else {
-            $('#login').show();
-            $('#logout').hide();
-        }
-    });
-
-    $('#login').on('click', function () {
-        $user.login();
-    });
-
-    $('#logout').on('click', function () {
-        $user.logout();
-    });
-
-    // Close the menu when an item is clicked.
-    $('#popupMenu li').on('click', function() {
-        $('#popupMenu').popup('close');
-    });
-
-};
 
     
 var _disqus = {
