91d4d806a2342869f7a55318ee270255b34c6832
Dashamir Hoxha <dashohoxha@gmail.com>
2015-02-23 09:22:38 +0100

    Changing language and vocabulary settings.

--- app.js
+++ app.js
@@ -1,9 +1,97 @@
 
 var $app = (function () {
 
-    $(document).ready(function () {
-        $( "body>[data-role='panel']" ).panel();
-    });
+    
+// The languages and vocabularies available.
+var _options = {
+    sq: {
+        name: 'Shqip',
+        webapp: 'https://l10n.org.al',
+        vocabularies: {
+            ICT_sq: 'Fjalori ICT',
+            huazime_sq: 'Fjalë të Huaja',
+        },
+    },
+    /*
+    en: {
+        name: 'English',
+        webapp: 'https://l10n.org.al',
+        vocabularies: {
+            ICT_sq: 'Fjalori ICT',
+        },
+    },
+    */
+};
+
+    
+var _settings = {
+    save: function () {
+        localStorage.setItem('vocabulary.lng', $config.lng);
+        localStorage.setItem('vocabulary.vocabulary', $config.vocabulary);
+    },
+
+    /** Load language and vocabulary from the local storage */
+    load: function () {
+        // Load lng.
+        var lng = localStorage.getItem('vocabulary.lng');
+        if (lng && lng != 'undefined') {
+            $config.lng = lng;
+        }
+
+        // Load vocabulary.
+        var vocabulary = localStorage.getItem('vocabulary.vocabulary');
+        if (vocabulary && vocabulary != 'undefined') {
+            $config.vocabulary = vocabulary;
+        }
+
+        // Update the panel.
+        this.update_panel();
+    },
+
+    update_panel: function () {
+        // Fill the list of languages.
+        var data = { languages: [] };
+        $.each(_options, function (lng, obj) {
+            data.languages.push({
+                lng: lng,
+                language: obj.name,
+                selected: (lng==$config.lng ? 'checked="checked"' : ''),
+            });
+        });
+        var tmpl = $('#tmpl-languages').html();
+        $('#languages').html(Mustache.render(tmpl, data)).trigger('create');
+
+        // Fill the list of vocabularies.
+        var data = { vocabularies: [] };
+        $.each(_options[$config.lng].vocabularies, function (id, name) {
+            data.vocabularies.push({
+                id: id,
+                name: name,
+                selected: (id==$config.vocabulary ? 'checked="checked"' : ''),
+            });
+        });
+        var tmpl = $('#tmpl-vocabularies').html();
+        $('#vocabularies').html(Mustache.render(tmpl, data)).trigger('create');
+
+        // Update layout of the panel.
+        $('#settings').trigger( "updatelayout" );
+
+        // Update config and settings when a vocabulary is selected.
+        $('.vocabulary').on('click', function () {
+            $config.vocabulary = this.value;
+            _settings.save();
+        });
+
+        // Update config and settings when a language is selected.
+        $('.lng').on('click', function () {
+            $config.lng = this.value;
+            for (var v in _options[$config.lng].vocabularies) break;
+            $config.vocabulary = v;
+            _settings.save();
+            _settings.update_panel();
+        });
+    },
+};
 
     
 var _menu = {
@@ -57,6 +145,12 @@ var _menu = {
  * do the things that are listed in the function.
  */
 $(document).on('pagecreate', '#vocabulary', function() {
+    // Load language and vocabulary from the local storage.
+    _settings.load();
+
+    // Setup menu items.
+    _menu.init();
+
     // Attach the function '_suggestions.list' to the event
     // 'filterablebeforefilter' from the list of suggestions.
     $('#suggestions').on('filterablebeforefilter', _suggestions.list);
@@ -65,9 +159,6 @@ $(document).on('pagecreate', '#vocabulary', function() {
     $('#next').on('click', function (event) {
         _term.get_random();
     });
-
-    // Setup menu items.
-    _menu.init();
 
     // Add a new term when the button is clicked.
     $('#add-new-term').on('click', _term.add);
@@ -597,5 +688,5 @@ var http_request = function(url, settings) {
     return request;
 };
 
-
+    
 })();
