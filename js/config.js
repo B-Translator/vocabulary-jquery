60c3ee43a9ca0d0ebe31d670e66c045684b2e507
Dashamir Hoxha <dashohoxha@gmail.com>
2015-02-25 01:35:44 +0100

    Fixing proxy login.

--- js/config.js
+++ js/config.js
@@ -11,8 +11,8 @@ var $config = {
     oauth2: {
 	app_id: 'vocabulary',
 	auth_flow: 'proxy',     // password | proxy
-	proxy_login: 'https://l10n.org.al/user/oauth2_login?proxy=true',
-	proxy_logout: 'https://l10n.org.al/user/logout',
+	proxy_login: 'https://l10n.org.al/user/proxy_login',
+	proxy_logout: 'https://l10n.org.al/user/proxy_logout',
 	token_endpoint: 'https://btranslator.org/oauth2/token',
 	client_id: 'vocabulary-jquery-ict-sq',
 	client_secret: 'Wadek9kAwgoovnepecOal8',
