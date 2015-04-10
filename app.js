9abca992a20a35a4c9daf9ffc7e47add3bc6c78d
Dashamir Hoxha <dashohoxha@gmail.com>
2015-04-10 09:26:37 +0200

    Changed custom Albanian keyboard layout.

--- app.js
+++ app.js
@@ -16,6 +16,44 @@ var _options = {
             layout: 'custom',
             customLayout: {
                 'normal' : [
+                    "q w e r t y u i o p",
+                    "a s d f g h j k l :",
+                    "z x c v b n m ' \" !",
+                    "\u00eb \u00e7 {bksp} {enter} {accept} {cancel}",
+                    "{shift} {alt} . {space}",
+                ],
+                'shift' : [
+                    "Q W E R T Y U I O P",
+                    "A S D F G H J K L ;",
+                    "Z X C V B N M ` \u00a6 ?",
+                    "\u00cb \u00c7 {tab} {enter} {accept} {cancel}",
+                    "{shift} {alt} , {space}",
+                ],
+                'alt' : [
+                    "0 1 2 3 4 5 6 7 8 9",
+                    "\u00a7 \u20ac $ # % _ - + = *",
+                    "( ) [ ] { } < > / \\",
+                    "& | ~ ^ {enter} {accept} {cancel}",
+                    "{shift} {alt} @ {space}",
+                ],
+                /*
+                'alt-shift' : [
+                    "0 1 2 3 4 5 6 7 8 9",
+                    "\u00a7 \u20ac $ # % _ - + = *",
+                    "( ) [ ] { } < > / \\",
+                    "& | ~ ^ {enter} {accept} {cancel}",
+                    "{shift} {alt} @ {space}",
+                ],
+                */
+            },
+        },
+
+        /*
+        //keyboard: { layout: 'albanian-qwerty' },
+        keyboard: {
+            layout: 'custom',
+            customLayout: {
+                'normal' : [
                     "` 1 2 3 4 5 6 7 8 9 0 - = {bksp}",
                     "{tab} q w e r t y u i o p \u00eb '",
                     "a s d f g h j k l ; \u00e7 # {enter}",
@@ -43,6 +81,7 @@ var _options = {
                     "{accept} {alt} {space} {alt} {cancel}"
                 ]           },
         },
+        */
     },
 };
 
