df89bc4e8c6b651a45003b13403acc0c26633e59
Dashamir Hoxha <dashohoxha@gmail.com>
2015-04-15 18:39:44 +0200

    Getting usage stats with google analytics (fixing #24).

--- app.js
+++ app.js
@@ -189,6 +189,27 @@ var _settings = {
     },
 };
 
+    // Google Analytics
+if ($config.ga_id) {
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+}
+else {
+    window.ga = function(){};
+}
+
+ga('create', $config.ga_id, 'auto');
+ga('send', 'pageview');
+
+$(document).ready(function () {
+    $('.ui-btn').on('click', function() {
+        var label = $(this)[0].textContent;
+        ga('send', 'event', 'button', 'click', label);
+    });
+});
+
     
 var _menu = {
     init: function () {
@@ -380,6 +401,10 @@ var _term = {
         // Get and display the list of translations.
         var sguid = Sha1.hash(term + $config.vocabulary);
         _translations.display(sguid);
+
+        // Track with Google Analytics.
+        ga('set', 'page', term);
+        ga('send', 'pageview');
     },
 
     /**
